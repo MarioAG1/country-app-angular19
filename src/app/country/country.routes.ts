7aa92f1393a289e74348909f3f4583a761852289
MarioAG <marioalvarezgonzalez21@gmail.com>
2025-12-06 17:36:38 +0100

    108. Creacion de layouts para country y cambios en el estilo de daisyui

--- src/app/country/country.routes.ts
+++ src/app/country/country.routes.ts
@@ -1,10 +1,21 @@
 import { Routes } from '@angular/router';
 import { ByCapitalPageComponent } from './pages/by-capital-page/by-capital-page.component';
+import { CountryLayout } from './layouts/CountryLayout/CountryLayout.component';
 
 export const countryRoutes: Routes = [
   {
     path: '',
-    component: ByCapitalPageComponent,
+    component: CountryLayout,
+    children: [
+      {
+        path: 'by-capital',
+        component: ByCapitalPageComponent,
+      },
+      {
+        path: '**',
+        redirectTo: '',
+      },
+    ],
   },
 ];
 
