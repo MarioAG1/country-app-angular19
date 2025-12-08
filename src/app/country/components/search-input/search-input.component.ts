4e3b0bdbb3d4a7d54ede5f59df905f08a34776a0
MarioAG <marioalvarezgonzalez21@gmail.com>
2025-12-08 13:57:46 +0100

    111 Tarea Input y Output basicos (placeholder es el input y el onSearch es el output, ademas de añadir las tablas para el diseño

---
+++ src/app/country/components/search-input/search-input.component.ts
@@ -0,0 +1,16 @@
+import { Component, input, output } from '@angular/core';
+
+@Component({
+  selector: 'country-search-input',
+  imports: [],
+  templateUrl: './search-input.component.html',
+})
+export class CountrySearchInput {
+  value = output<string>();
+  placeholder = input<string>();
+
+  // Esta es 1 opcion, la 2 opcion es directamente desde el HTML
+  // onSearch(value: string) {
+  //   this.searchValue.emit(value);
+  // }
+}
