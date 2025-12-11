d4b23f14b3763adffb81c0fe26749783e821545e
MarioAG <marioalvarezgonzalez21@gmail.com>
2025-12-11 14:51:06 +0100

    125. Reactividad con Resources, es la clase anterior, pero reduciendolo con resource, que es experimental

--- src/app/country/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/country/pages/by-capital-page/by-capital-page.component.ts
@@ -1,9 +1,10 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, resource, signal } from '@angular/core';
 import { SearchInputComponent } from '../../components/search-input/search-input.component';
 import { CountryListComponent } from '../../components/list/country-list.component';
 import { CountryService } from '../../services/country.service';
 import { RESTCountry } from '../../interfaces/rest-countries.interface';
 import { Country } from '../../interfaces/country.interface';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-by-capital-page',
@@ -12,31 +13,46 @@ import { Country } from '../../interfaces/country.interface';
 })
 export class ByCapitalPageComponent {
   countryService = inject(CountryService);
-  isLoading = signal(false);
-  isError = signal<string | null>(null);
 
-  // 1 Cambiamos a tipo RestCountry igual que el servicio, para evitar el error de asignar argumentos
-  // Tarea Mapper, cambiamos el RestCountry por la interfaz neustra
-  countries = signal<Country[]>([]);
+  // 125. Esto remplazaria todo lo de abajo
+  query = signal('');
+  countryResource = resource({
+    request: () => ({ query: this.query() }),
+    loader: async ({ request }) => {
+      if (!request.query) {
+        return [];
+      }
+      return await firstValueFrom(
+        this.countryService.searchByCapital(request.query)
+      );
+    },
+  });
 
-  onSearch(query: string) {
-    if (this.isLoading() === true) {
-      return;
-    }
+  // isLoading = signal(false);
+  // isError = signal<string | null>(null);
 
-    this.isLoading.set(true);
-    this.isError.set(null);
+  //  1 Cambiamos a tipo RestCountry igual que el servicio, para evitar el error de asignar argumentos
+  //  Tarea Mapper, cambiamos el RestCountry por la interfaz neustra
+  // countries = signal<Country[]>([]);
 
-    this.countryService.searchByCapital(query).subscribe({
-      next: (countries) => {
-        this.isLoading.set(false);
-        this.countries.set(countries);
-      },
-      error: (error) => {
-        this.isLoading.set(false);
-        this.countries.set([]);
-        this.isError.set(error);
-      },
-    });
-  }
+  // onSearch(query: string) {
+  //   if (this.isLoading() === true) {
+  //     return;
+  //   }
+
+  //   this.isLoading.set(true);
+  //   this.isError.set(null);
+
+  //   this.countryService.searchByCapital(query).subscribe({
+  //     next: (countries) => {
+  //       this.isLoading.set(false);
+  //       this.countries.set(countries);
+  //     },
+  //     error: (error) => {
+  //       this.isLoading.set(false);
+  //       this.countries.set([]);
+  //       this.isError.set(error);
+  //     },
+  //   });
+  // }
 }
