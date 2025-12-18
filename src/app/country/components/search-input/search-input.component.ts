40392a9023f2d578d9f1ff317bc2068ee8aa586c
MarioAG <marioalvarezgonzalez21@gmail.com>
2025-12-18 13:58:38 +0100

    137. Debounce, busquedas automaticas

--- src/app/country/components/search-input/search-input.component.ts
+++ src/app/country/components/search-input/search-input.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, output } from '@angular/core';
+import { Component, effect, input, output, signal } from '@angular/core';
 
 @Component({
   selector: 'country-search-input',
@@ -8,6 +8,20 @@ import { Component, input, output } from '@angular/core';
 export class SearchInputComponent {
   value = output<string>();
   placeholder = input<string>();
+  debounceTime = input.required<number>();
+  inputValue = signal('');
+
+  debounceEffect = effect((onCleanUp) => {
+    //Cuando se detecta que hay una señal y esta cambie, activara el efecto
+    const value = this.inputValue();
+    const timeout = setTimeout(() => {
+      this.value.emit(value);
+    }, this.debounceTime());
+
+    onCleanUp(() => {
+      clearTimeout(timeout);
+    });
+  });
 
   // Esta es 1 opcion, la 2 opcion es directamente desde el HTML
   // onSearch(value: string) {
